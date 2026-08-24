008794190b8e1eed26ddfeb557b0f7b223a6711d
Aaryan Junnarkar <aaryan.junnarkar@gmail.com>
2026-08-24 21:04:46 +0530

    Changed functions around

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import resources as report
 
 # Constants
 REPORT = report.resources
-OUT_OF_RESOURCES = False
+IS_RUNNING = False
 ACTIVE_MENU = menu.MENU
 
 # Coins
@@ -29,10 +29,18 @@ def resources():
 
 # Print from the menu
 def cost(choice):
+
     # Pick the cost from the menu
     return ACTIVE_MENU[choice]['cost']
 
-while not OUT_OF_RESOURCES:
+def billing(actual_cost, user_money):
+    if user_money >= actual_cost:
+        return True
+    else:
+        print("Insufficient money")
+        return False
+
+while not IS_RUNNING:
 
     # Prompt user by asking what they want
     user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
@@ -40,21 +48,21 @@ while not OUT_OF_RESOURCES:
     # Turn off the Coffee Machine if user enters "off"
     if user_choice == "off":
         print("Turning off the machine...")
-        OUT_OF_RESOURCES = True
+        IS_RUNNING = True
 
     # Print the report of what's available
     elif user_choice == "report":
         resources()
 
     # If they select coffee
-    elif user_choice == 'espresso' or 'latte' or 'cappuccino':
+    elif user_choice == 'espresso' or user_choice =='latte' or user_choice == 'cappuccino':
         print("Please insert coins.")
 
         # Ask user for coins
-        coin_quart = float(input("How many quarters?: "))
-        coin_dim = float(input("How many dimes?: "))
-        coin_nick = float(input("How many nickles?: "))
-        coin_penn = float(input("How many pennies?: "))
+        coin_quart = int(input("How many quarters?: "))
+        coin_dim = int(input("How many dimes?: "))
+        coin_nick = int(input("How many nickles?: "))
+        coin_penn = int(input("How many pennies?: "))
 
         # Store the price of the coffee
         store_value = cost(user_choice)
@@ -62,11 +70,17 @@ while not OUT_OF_RESOURCES:
         # Call function
         money = calc(coin_quart, coin_dim, coin_nick, coin_penn)
 
-        # Print the money
-        print(f"Here is ${money - store_value} in change.")
+        # Check if the user has enough money for this
+        has_money = billing(store_value, money)
 
-        # Print the choice
-        print(f"Here is you {user_choice} ☕ Enjoy!")
+        if has_money:
+            continue
+        else:
+            # Print the money
+            print(f"Here is ${round(money - store_value, 2)} in change.")
+
+            # Print the choice
+            print(f"Here is you {user_choice} ☕ Enjoy!")
 
     else:
         print("Incorrect choice!")
